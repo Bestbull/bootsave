c643278c31434c550a78952ae896850757479654
wzc520pyf <1528857653@qq.com>
2021-04-14 13:48:23 +0800

    救援详情

--- help-family/pages/applying/applying.js
+++ help-family/pages/applying/applying.js
@@ -5,14 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    first_date:"2021-04-03",
+    first_time:"09: 35: 45",
+    isfind:"搜寻中",
+    findingColor:"#F99902",
+    unisfind:"未执行",
+    unfindingColor:"#3B9DEC",
+    message:"发现一位疑似",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    setTimeout(function(){
+      wx.showModal({
+        title:"走失时间太长",
+        confirmColor:'#EE2A2A',
+        confirmText:'立即报警',
+        cancelText:'取消',
+        success:function(res){
+          if(res.confirm){
+            // 唤起拨号
+            wx.makePhoneCall({
+              phoneNumber: '110',
+              success:function(){
+                console.log("拨号成功!")
+              },
+              fial:function(){
+                wx.showToast({
+                  title: '拨号失败',
+                  confirmText:'请手动报警',
+                  duration:1500
+                })
+              }
+            })
+          }else if(res.cancel){
+            wx.showToast({
+              title: '您取消了操作',
+              icon:'success',
+              duration:1500
+            })
+          }
+        }
+      })
+    },2000)
   },
 
   /**
